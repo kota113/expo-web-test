699434d4784143c960b31427afcbe93b875f1cc6
kota113 <kota113@keio.jp>
2025-12-05 19:56:59 +0900

    feat: add necessary files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import '../global.css';
+import '../styles/web-global.css';
 
 export const unstable_settings = {
   anchor: 'home',
